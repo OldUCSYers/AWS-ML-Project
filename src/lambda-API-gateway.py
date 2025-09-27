15e91a533e80fa17638327c9f9587bd9f97ec5e9
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-09-27 13:33:54 +0630

    Create lambda-API-gateway.py

---
+++ src/lambda-API-gateway.py
@@ -0,0 +1,4 @@
+curl -X POST \
+  https://wcv3oyt4sj.execute-api.ap-southeast-1.amazonaws.com/default/lambda-api-gateway \
+  -H "Content-Type: application/json" \
+  -d '{"input": "Hello Thet Su"}'
