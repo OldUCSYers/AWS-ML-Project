785a1d4bfe27f1b3511761dd6ae2f16dc5c215dd
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-09-27 13:35:28 +0630

    Update lambda-API-gateway.py

--- src/lambda-API-gateway.py
+++ src/lambda-API-gateway.py
@@ -1,4 +1,4 @@
 curl -X POST \
   https://ct2vj553ck.execute-api.ap-southeast-1.amazonaws.com/default/data-validation \
   -H "Content-Type: application/json" \
-  -d '{"input": "Hello Thet Su"}'
+  -d '{"input": "Hello World!"}'
